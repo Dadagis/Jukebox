65211cbaff09ba81d9d2e6839515c96216b4175c
David <davidobob@laposte.net>
2020-03-26 12:12:02 +0100

    customized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-  get 'rooms/show'
-  get 'rooms/create'
+  resources :rooms, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
