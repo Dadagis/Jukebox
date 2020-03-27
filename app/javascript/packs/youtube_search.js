13b515fd0aa0b89ca9d5714fdd76f29c17c6e18d
David <davidobob@laposte.net>
2020-03-27 19:32:49 +0100

    event listener and fetching are working + cards display

--- app/javascript/packs/youtube_search.js
+++ app/javascript/packs/youtube_search.js
@@ -1,20 +1,33 @@
-const form = document.querySelector('.salut');
-console.log("bonjour");
-console.log(form);
-console.log("salut");
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   console.log("salut");
-// })
+const eventListener = () => {
+  const form = document.querySelector('#search-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const query = document.querySelector('#form').value;
+    searchVideo(query);
+  })
+}
 
 
-const searchVideo = () =>{
-  const querry = "squeezie"
-  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${querry}&type=video&key=${process.env.YOUTUBE_API_KEY}`)
+const results = document.querySelector('#results');
+
+
+const searchVideo = (query) => {
+  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${process.env.YOUTUBE_API_KEY}`)
   .then(response => response.json())
   .then((data) => {
-    console.log(data);
+    data.items.forEach((item) => {
+      let insertInfos = `<div class="flex justify-center col-span-1 transform hover:scale-110 duration-300 ease-in-out">
+      <div class="flex w-1/2">
+      <img class="w-56" src="${item.snippet.thumbnails.medium.url}" alt="thumbnail">
+      <div class="border border-gray-400 bg-white p-4 flex flex-col justify-center w-full">
+      <div class="text-gray-900 font-bold text-base mb-2">${item.snippet.title}</div>
+      </div>
+      </div>
+      </div>
+      </div>`;
+      results.insertAdjacentHTML("afterend", insertInfos);
+    })
   });
 }
 
-export { searchVideo };
+export { eventListener };
