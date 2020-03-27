060b6306c08e92bbb79701e752e052298a3c6710
David <davidobob@laposte.net>
2020-03-27 14:07:56 +0100

    dotenv gem

--- app/javascript/packs/youtube_search.js
+++ app/javascript/packs/youtube_search.js
@@ -1,7 +1,8 @@
 const searchVideo = () =>{
-  const key = process.env.YOUTUBE_API_KEY;
+  const key = process.env.YOUTUBE_API_KEY
   console.log(key);
-  // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=squeezie&type=video&key=${process.env.YOUTUBE_API_KEY}`)
+  console.log("salut")
+  // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${querry}&type=video&key=${process.env.YOUTUBE_API_KEY}`)
   // .then(response => response.json())
   // .then((data) => {
   //   console.log(data);
