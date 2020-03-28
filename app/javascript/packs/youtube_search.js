15e3737a01d8af51f27b280989c39919ac2ecfc5
David <davidobob@laposte.net>
2020-03-28 12:22:34 +0100

    added clear cards function before every research

--- app/javascript/packs/youtube_search.js
+++ app/javascript/packs/youtube_search.js
@@ -1,22 +1,33 @@
+const results = document.querySelector('#results');
+
 const eventListener = () => {
   const form = document.querySelector('#search-form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const query = document.querySelector('#form').value;
-    searchVideo(query);
+    cleanAndFetch();
   })
 }
 
-
-const results = document.querySelector('#results');
-
+const cleanAndFetch = () => {
+  const query = document.querySelector('#form').value;
+  const cards = Array.from(document.querySelectorAll('.video-card'));
+  if (cards.length === 0) {
+    searchVideo(query);
+  } else {
+    cards.forEach((card) => {
+      card.remove();
+    })
+    searchVideo(query);
+  }
+}
 
 const searchVideo = (query) => {
   fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${process.env.YOUTUBE_API_KEY}`)
   .then(response => response.json())
   .then((data) => {
     data.items.forEach((item) => {
-      let insertInfos = `<div class="flex justify-center col-span-1 transform hover:scale-110 duration-300 ease-in-out">
+      let insertInfos = `<li class="video-card">
+      <div class="flex justify-center col-span-1 transform hover:scale-110 duration-300 ease-in-out mb-4">
       <div class="flex w-1/2">
       <img class="w-56" src="${item.snippet.thumbnails.medium.url}" alt="thumbnail">
       <div class="border border-gray-400 bg-white p-4 flex flex-col justify-center w-full">
@@ -24,8 +35,9 @@ const searchVideo = (query) => {
       </div>
       </div>
       </div>
-      </div>`;
-      results.insertAdjacentHTML("afterend", insertInfos);
+      </div>
+      </li>`;
+      results.insertAdjacentHTML("afterbegin", insertInfos);
     })
   });
 }
